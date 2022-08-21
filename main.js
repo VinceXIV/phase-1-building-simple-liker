8769c2d721618c25da4b7e68b01b13a82b2b6d76
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-21 23:16:23 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let likeGlyphs = document.querySelectorAll('.like-glyph')
 
+likeGlyphs.forEach(likeGlyph => {
+  likeGlyph.addEventListener('click', ()=>{
+    mimicServerCall()
+    .then(result => {
+      if(likeGlyph.textContent == FULL_HEART){
+        likeGlyph.textContent = EMPTY_HEART;
+        likeGlyph.classList.remove('activated-heart')
+      }else {
+        likeGlyph.textContent = FULL_HEART
+        likeGlyph.classList.add('activated-heart')
+      }
+    })
+    .catch(error => {
+      console.log("error message", error)
+      const modal = document.getElementById('modal');
+      modal.classList.remove('hidden')
+      setTimeout(model => {
+        modal.classList.add('hidden')
+      }, 3000)
+  
+      modal.querySelector('#modal-message').textContent = error
+  
+      
+    })
+  })
+})
 
 
 
@@ -14,7 +41,7 @@ const FULL_HEART = '♥'
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+      let isRandomFailure = Math.random() < .2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
